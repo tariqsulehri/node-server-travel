5197aab7d505039f4b29fbc4b246cbe1e68fa180
Administrator <Administrator@TS>
2021-04-17 19:10:40 +0500

    home Route changed

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router(); // instead this will work.
 
 router.post('/', async (req, res) => {
-    res.send("Home");
+    res.send("Home Post");
 });
 
 router.get('/', async (req, res) => {
-    res.send("Home");
+    res.send("Travel Api Server....");
 });
 
 module.exports = router;
